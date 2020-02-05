e7d6f7efc3d49bc97a74cd540ceacabd945a19c3
davidfu0826 <42645812+davidfu0826@users.noreply.github.com>
2020-02-05 14:46:29 +0100

    Update utility_functions.py

--- utility_functions.py
+++ utility_functions.py
@@ -1,6 +1,8 @@
 from skimage import io, color
 import matplotlib.pyplot as plt
 from skimage import exposure
+from skimage.exposure import rescale_intensity
+import cv2
 import pylab
 import skimage.measure
 import numpy as np
@@ -60,9 +62,10 @@ def visualize_convolution(original_image, kernel):
   axs[0].axis('off')
 
   # Convoluted image
-  convolved_image = convolve2d(original_image, kernel)
-  convolved_image = skimage.measure.block_reduce(convolved_image, (2,2), np.max) # Maxpooling
-  convolved_image = exposure.equalize_adapthist(convolved_image/np.max(np.abs(convolved_image)), clip_limit=0.03)
+  convolved_image = convolve(original_image, kernel)
+  #convolved_image = convolve2d(original_image, kernel)
+  #convolved_image = skimage.measure.block_reduce(convolved_image, (2,2), np.max) # Maxpooling
+  #convolved_image = exposure.equalize_adapthist(convolved_image/np.max(np.abs(convolved_image)), clip_limit=0.03)
   axs[1].imshow(convolved_image, cmap=plt.cm.gray)
   axs[1].set_title("Convolved image")
   axs[1].axis('off')
@@ -86,6 +89,40 @@ def convolve2d(image, kernel):
             # element-wise multiplication of the kernel and the image
             output[y,x]=(kernel*image_padded[y:y+kernel.shape[1],x:x+kernel.shape[0]]).sum()        
     return output
+  
+def convolve(image, kernel):
+	# grab the spatial dimensions of the image, along with
+	# the spatial dimensions of the kernel
+	(iH, iW) = image.shape[:2]
+	(kH, kW) = kernel.shape[:2]
+	# allocate memory for the output image, taking care to
+	# "pad" the borders of the input image so the spatial
+	# size (i.e., width and height) are not reduced
+	pad = (kW - 1) // 2
+	image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
+		cv2.BORDER_REPLICATE)
+	output = np.zeros((iH, iW), dtype="float32")
+ 	# loop over the input image, "sliding" the kernel across
+	# each (x, y)-coordinate from left-to-right and top to
+	# bottom
+	for y in np.arange(pad, iH + pad):
+		for x in np.arange(pad, iW + pad):
+			# extract the ROI of the image by extracting the
+			# *center* region of the current (x, y)-coordinates
+			# dimensions
+			roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+			# perform the actual convolution by taking the
+			# element-wise multiplicate between the ROI and
+			# the kernel, then summing the matrix
+			k = (roi * kernel).sum()
+			# store the convolved value in the output (x,y)-
+			# coordinate of the output image
+			output[y - pad, x - pad] = k
+	# rescale the output image to be in the range [0, 255]
+	output = rescale_intensity(output, in_range=(0, 255))
+	output = (output * 255).astype("uint8")
+	# return the output image
+	return output
 
 def scatter_plot(images, labels, label_to_article, title='PCA of Fasion-MNIST Dataset', nbr_samples=400):
   fig = plt.figure(figsize=(14, 10))
